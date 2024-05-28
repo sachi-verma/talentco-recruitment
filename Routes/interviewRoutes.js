4e8f7f9db412c17d1c1ec0992f41bb7829108bf7
Sachi <sachiverma00@gmail.com>
2024-05-28 17:37:27 +0530

    Update interviewRoutes.js

--- Routes/interviewRoutes.js
+++ Routes/interviewRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const interviewController = require('../Controllers/interviewController');
 
-
+router.get('/candidates', interviewController.getCandidates);
 router.get('/getinterview', interviewController.getInterviewSchedule);
 router.post('/addinterview', interviewController.addInterviewSchedule);
 router.patch('/editinterview/:id', interviewController.editInterviewSchedule);
